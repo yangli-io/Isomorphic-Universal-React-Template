1e484fbbcde608efff06cdf76370418327d4ea1b
Yang Li <yangli@yangs-mbp-2.home>
2015-12-20 13:28:16 +1100

    upate

---
+++ test-setup.js
@@ -0,0 +1,33 @@
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+
+global.expect = expect;
+chai.Should();
+chai.use(sinonChai);
+
+import jsdom from 'jsdom';
+
+// setup the simplest document possible
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+
+// get the window object out of the document
+const win = doc.defaultView;
+
+// set globals for mocha that make access to document and window feel
+// natural in the test environment
+global.document = doc;
+global.window = win;
+
+// take all properties of the window object and also attach it to the
+// mocha global object
+propagateToGlobal(win);
+
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+	for (let key in window) {
+		if (!window.hasOwnProperty(key)) continue;
+		if (key in global) continue;
+
+		global[key] = window[key]
+	}
+}
