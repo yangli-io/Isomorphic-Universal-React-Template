1d4d3101768a833b23baea2e44c0434f32fa3ef2
Yang Li <yangli@yangs-mbp-2.home>
2015-12-31 13:43:23 +1100

    fix serverDOM

--- src/server/serverDOM.js
+++ src/server/serverDOM.js
@@ -1,32 +1,25 @@
 import jsdom from 'jsdom';
-import { render } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
+
+const dom = '<!doctype html><html><body><div id="root"></div></body></html>';
+
+//For any global dom activities
+const doc = jsdom.jsdom(dom);
+const win = doc.defaultView;
+global.document = doc;
+global.window = win;
+(function (window) {
+	for (let key in window) {
+		if (!window.hasOwnProperty(key)) continue;
+		if (key in global) continue;
+
+		global[key] = window[key]
+	}
+})(win);
 
 export default function(reactElement) {
-    // setup the simplest document possible
-	const doc = jsdom.jsdom('<!doctype html><html><body><div id="root"></div></body></html>')
-
-    // get the window object out of the document
-	const win = doc.defaultView;
-
-   // set globals for mocha that make access to document and window feel
-   // natural in the test environment
-	const document = doc;
-	const window = win;
-
-	// take all properties of the window object and also attach it to the
-	// mocha global object
-	propagateToGlobal(win);
-
-    // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-	function propagateToGlobal (window) {
-		for (let key in window) {
-			if (!window.hasOwnProperty(key)) continue;
-			if (key in global) continue;
-
-			global[key] = window[key]
-		}
-	}
-
-	const reactRoot = window.document.getElementById('root');
+	//renders the dom
+	let doc = jsdom.jsdom(dom);
+	let reactRoot = doc.defaultView.document.getElementById('root');
 	render(reactElement, reactRoot);
 }
