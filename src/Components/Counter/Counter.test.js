f318aabc961b71123bc95573f42a80b12aad68dc
yangli1990 <yangli1990@live.com.au>
2016-01-02 07:02:33 +1100

    add more tests

--- src/Components/Counter/Counter.test.js
+++ src/Components/Counter/Counter.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import TestUtils from 'react-addons-test-utils';
 import { renderIntoDocument } from 'react-addons-test-utils';
 import { findDOMNode } from 'react-dom';
-import { Counter } from './Counter';
+const proxyquire = require('proxyquire').noCallThru();
+
+const initializeSpy = sinon.spy();
+const incrementSpy = sinon.spy();
+const decrementSpy = sinon.spy();
+const counterActionsStub = {
+	increment: incrementSpy,
+	decrement: decrementSpy,
+	initialize: initializeSpy
+};
+
+const Counter = proxyquire('./Counter', {
+	'../../actions/counterActions': counterActionsStub
+}).Counter;
 
 describe('Counter', () => {
 	let component;
@@ -15,4 +29,30 @@ describe('Counter', () => {
 	it('should be renderable', () => {
 		expect(renderedElement).to.not.be.null;
 	});
+
+	it('should have called the initialize action', () => {
+		expect(initializeSpy).to.have.been.called;
+	});
+
+	describe('use clicks + button', () => {
+		before(() => {
+			const buttons = TestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+			TestUtils.Simulate.click(buttons[0]);
+		});
+
+		it('should have called the increment action', () => {
+			expect(incrementSpy).to.have.been.called;
+		});
+	});
+
+	describe('use clicks - button', () => {
+		before(() => {
+			const buttons = TestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+			TestUtils.Simulate.click(buttons[1]);
+		});
+
+		it('should have called the increment action', () => {
+			expect(decrementSpy).to.have.been.called;
+		});
+	})
 });
