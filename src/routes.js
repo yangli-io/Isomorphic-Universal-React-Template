181032abe6cb7d7ded58465042b57e5d200ad62c
yangli1990 <yangli1990@live.com.au>
2016-01-01 19:06:30 +1100

    really fix routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import App from './components/App';
 import {Router, Route} from 'react-router';
 import history from './shared';
+import Counter from './components/Counter/Counter';
 
 export default (
 	<Route path="/" component={App}>
