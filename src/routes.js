72853febf3251e5b34e6654915c8f9f55a9b316e
yangli1990 <yangli1990@live.com.au>
2016-01-01 19:04:24 +1100

    fix routes again for linux

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
 import history from './shared';
-import Counter from 'components/Counter/Counter';
 
 export default (
 	<Route path="/" component={App}>
