a6062d9bcc53c49e62ddd53c637003ce450f6fee
yangli1990 <yangli1990@live.com.au>
2016-01-16 16:49:11 +1100

    upcase folder

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import App from './components/App';
+import App from './Components/App';
 import {Router, Route} from 'react-router';
 import history from './shared';
-import Counter from './components/Counter/Counter';
+import Counter from './Components/Counter/Counter';
 
 export default (
 	<Route path="/" component={App}>
