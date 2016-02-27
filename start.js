1ad28fb6e968277826fffed30d85aeba1a4b6938
yangli1990 <yangli1990@live.com.au>
2016-02-27 23:35:25 +1100

    use nodemon

--- start.js
+++ start.js
@@ -1,16 +1,6 @@
+global.isBrowser = false;
 require("babel-polyfill");
 require("babel-core/register");
-
-global.isBrowser = false;
-
-if (process.env.NODE_ENV !== "production") {
-	try {
-		if (!require("piping")({hook: true, includeModules: false})) {
-			return;
-		}
-	} catch (e) {
-		console.log('production environment - no piping');
-	}
-}
-
 require("./src/server/server");
+
+
